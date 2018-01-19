9e5782ee01b1dd7ae1ee3dc06a3bbdf7458a57d6
Jarkko Selkäinaho <jarkko.selkainaho@10duke.com>
2018-01-19 11:32:22 +0200

    NuGet configurations

--- Tenduke.EntitlementClient/Properties/AssemblyInfo.cs
+++ Tenduke.EntitlementClient/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tenduke.EntitlementClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("10Duke Entitlement service client library for .NET desktop applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("10Duke Software")]
 [assembly: AssemblyProduct("Tenduke.EntitlementClient")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.2")]
