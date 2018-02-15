07596dac7db8cd81edef673d6e485244c192e3fd
Jarkko Selkäinaho <jarkko.selkainaho@10duke.com>
2018-02-15 17:11:56 +0200

    Asking the server to hide "remember me" checkbox for sign-ons using the embedded browser

--- Tenduke.EntitlementClient/Authorization/BrowserBasedAuthorization.cs
+++ Tenduke.EntitlementClient/Authorization/BrowserBasedAuthorization.cs
@@ -109,6 +109,9 @@ namespace Tenduke.EntitlementClient
             var query = HttpUtility.ParseQueryString(uriBuilder.Query);
             query["response_type"] = GetResponseType();
             query["client_id"] = OAuthConfig.ClientID;
+            // Always ask the server to hide "Remember me" checkbox, authorization persistence shall be
+            // controlled by the application.
+            query["showRememberMe"] = "false";
 
             if (OAuthConfig.RedirectUri != null)
             {
