43e3db5785ab66279bffaf673e4cbcdf549ce231
Jarkko Selkäinaho <jarkko@10duke.com>
2018-05-15 11:22:43 +0300

    Removed contents of packages directory from version control

--- packages/WireMock.Net.1.0.2.11/src/WireMock.Net/MappingRegistrationCallback.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WireMock
-{
-    /// <summary>
-    /// The registration callback.
-    /// </summary>
-    /// <param name="mapping">The mapping.</param>
-    public delegate void RegistrationCallback(Mapping mapping);
-}
